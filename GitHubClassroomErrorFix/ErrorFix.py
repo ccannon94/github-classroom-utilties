3d4d38c9225a684d103592564c7bba67f53fec44
Chris Cannon <chris.c.canon@gmail.com>
2017-10-25 20:38:59 -0400

    Update script to pull first

--- GitHubClassroomErrorFix/ErrorFix.py
+++ GitHubClassroomErrorFix/ErrorFix.py
@@ -1,16 +1,19 @@
 import os
 
-solutiondir = '/Users/CCannon/Documents/COMP167/MP2-Solution/prog2in.txt'
+solutiondir = '/Users/CCannon/Downloads/GUIAddendum.pdf'
 repodir = '/Users/CCannon/Documents/COMP167/MP2-Student-Repos'
 
 for subdir in os.walk(repodir):
     for i in subdir:
         if type(i) == str:
             if len(i.split("/")) == 7:
+                os.chdir(i)
+                os.system("git checkout master")
+                os.system("git pull")
                 copyCmd = "cp {} {}".format(solutiondir, i)
                 os.system(copyCmd)
-                os.chdir(i)
-                os.system("git commit -a -m \"update input file\"")
+                os.system("git add .")
+                os.system("git commit -m \"Add extra credit addendum\"")
                 os.system("git push")
 
                 #print copyCmd
