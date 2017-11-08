a174edef5b8a5c73b5ea48da0af18a2e372594a6
Chris Cannon <chris.c.canon@gmail.com>
2017-11-08 14:14:10 -0500

    Make ErrorFix run with command line arguments

--- GitHubClassroomErrorFix/ErrorFix.py
+++ GitHubClassroomErrorFix/ErrorFix.py
@@ -1,20 +1,23 @@
 import os
+import sys
 
-solutiondir = '/Users/CCannon/Downloads/GUIAddendum.pdf'
-repodir = '/Users/CCannon/Documents/COMP167/MP2-Student-Repos'
+if(len(sys.argv) == 3):
+    solutionPath = sys.argv[1]
+    repoDir = sys.argv[2]
+else:
+    print "Invalid command line arguments, please run: ErrorFix.py [absolute path of solution] [absolute path of directory containing student repositories]"
+    sys.exit(0)
 
-for subdir in os.walk(repodir):
+
+for subdir in os.walk(repoDir):
     for i in subdir:
         if type(i) == str:
             if len(i.split("/")) == 7:
                 os.chdir(i)
                 os.system("git checkout master")
                 os.system("git pull")
-                copyCmd = "cp {} {}".format(solutiondir, i)
+                copyCmd = "cp {} {}".format(solutionPath, i)
                 os.system(copyCmd)
                 os.system("git add .")
-                os.system("git commit -m \"Add extra credit addendum\"")
+                os.system("git commit -m \"Add additional assignment document\"")
                 os.system("git push")
-
-                #print copyCmd
-                #print "git commit -a -m \"update input file\""
