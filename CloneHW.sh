f7f55a9520d53f0d9fa62fab28e1f6de25e2384c
Chris Cannon <chris.c.canon@gmail.com>
2018-02-19 14:30:30 -0500

    Create a clone script specific to COMP167 Spring 2018

---
+++ CloneHW.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+#     GitHub Classroom clone homework
+#
+#		INSTRUCTIONS:
+#		1) Run this file specifying the homework name and optional tagname
+# ---------------------------------------------------------------------------
+if [ -z "$1" ]
+then
+   echo Usage: cloneHW [homework_name]
+   exit 1
+fi
+
+currentdir=$(pwd)
+
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-akacquaye.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-christopherallis.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Amponsah17.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-krbailey.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-taylorbailey.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-ynbaldwin.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-opmbanks.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Evin-Gregory.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Klblow.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-CBroomes.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-DAndreBrown.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-JayNeedsHelp.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-ThandiBrown.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-wrbrown1.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-kgbullard.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Rdbunch.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-dburnett99.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-YooByrd.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-dkcabbagestalk.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-isaiah0.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-dfcardona.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-cherrymalia.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-OluchiC.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-kacoates.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-czcroom.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-pocrump.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Deriech.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-jxdawson.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-jndecote.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-TakyahL.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-eadyhtc.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Bryanel565.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-jayfulton65.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-CamiliaGaitan.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-sgarcia3.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-rdgeorge1.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-JGilmore98.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-ygjadi.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Jjguzman1.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-krhargra.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-cmhawkinds.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-jhernandezcampillo.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-JJHV36.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-slhill13.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Doetheman.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-mahoward99.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-srosehudson.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-ashonjohnson13.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-sonic536.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-czlanier.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-kmlissa.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-lclittle.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-LondonWesley.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-WesCLong.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-tdmayo1.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-zmiller1.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Bennyhuncho.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Quay1234.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-jtmoore3.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-jeffmoore2.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Rjnelson021.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-NailahNolley.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Morefiendz.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-koogachi.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-orekunrin.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-cxpreston.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-DamaniRagin.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-JadelynRay.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Shondale.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-ChrisRhone.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-kjrosa.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-Nikell286.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-sonu0909.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-BraxtonNCAT.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-shadowsword2143.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-JaredAlexSolo.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-astepter365.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-reggies712.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-adstokes.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-tjstreet4.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-mikalyyn.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-srtaylo1.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-dariusmini.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-ereewa3.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-acwalke2.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-mileswalker51.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-clwallace.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-TheRealSimz.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-softwaregirl1994.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-trose36.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-xavierwilliams1.git
+git clone https://github.com/NCAT-COMP167-Spring2018/$1-jmyouance.git
