42b61dccc491e69e627b7263b9e702a16c803f20
Chris Cannon <chris.c.canon@gmail.com>
2018-04-04 14:17:22 -0400

    Duh

--- addFile.py
+++ addFile.py
@@ -2,24 +2,26 @@ import os
 import sys
 import subprocess
 
-if(len(sys.argv) == 3):
+if(len(sys.argv) == 4):
     repoDir = sys.argv[1]
     solutionPath = sys.argv[2]
+    assignmentName = sys.argv[3]
 else:
-    print "Invalid command line arguments, please run: addFile.py [absolute path of solution] [absolute path of directory containing student repositories]"
+    print "Invalid command line arguments, please run: addFile.py [absolute path of directory containing student repositories] [absolute path of solution] [name-of-assignment-as-it-appears-in-repository-names]"
     sys.exit(0)
 
 
 for subdir in os.walk(repoDir):
     for i in subdir:
         print i
-        if type(i) == str:
+        if (type(i) == str) & (i.find(assignmentName) != -1):
             if subprocess.call(['git', '-C', i, 'status'], stderr=subprocess.STDOUT, stdout = open(os.devnull, 'w')) == 0:
                 os.chdir(i)
-                os.system("git checkout master")
-                os.system("git pull")
-                copyCmd = "cp {} {}".format(solutionPath, i)
-                os.system(copyCmd)
-                os.system("git add .")
-                os.system("git commit -m \"Add additional assignment document\"")
-                os.system("git push")
+                print "Top level git repo"
+                #os.system("git checkout master")
+                #os.system("git pull")
+                #copyCmd = "cp {} {}".format(solutionPath, i)
+                #os.system(copyCmd)
+                #os.system("git add .")
+                #os.system("git commit -m \"Add additional assignment document\"")
+                #os.system("git push")
