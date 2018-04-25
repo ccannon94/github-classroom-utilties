2cf32892a649344f25c7135d38a82b2294c9bbd0
Chris Cannon <chris.c.canon@gmail.com>
2018-04-25 13:49:46 -0400

    Clones and organizes assignments, ready to copy files

--- MossSetup.py
+++ MossSetup.py
@@ -4,25 +4,39 @@ import os
 import sys
 import subprocess
 
-if(len(sys.argv) == 5):
+if(len(sys.argv) == 6):
     classroomName = sys.argv[1]
     assignmentName = sys.argv[2]
     rosterPath = sys.argv[3]
     cloneAssignmentScriptPath = sys.argv[4]
+    destinationPath = sys.argv[5]
 else:
-    print ("Invalid command line arguments, please run: MossSetup.py [name of classroom][name of assignment] [path to roster]")
+    print ("Invalid command line arguments, please run: MossSetup.py [name of classroom][name of assignment] [path to roster] [path to CloneAssignment.py script] [path to empty directory that will hold student work]")
     sys.exit(0)
 
-cloneAllCommand = "python3 {} {} {} {}".format(cloneAssignmentScriptPath, classroomName, assignmentName, rosterPath)
+#TODO: Uncomment when testing complete
+#os.mkdir("GitHub-Repos")
+#os.chdir("GitHub-Repos")
+#cloneAllCommand = "python3 {} {} {} {}".format(cloneAssignmentScriptPath, classroomName, assignmentName, rosterPath)
 
-subprocess.call(cloneAllCommand, shell=True)
+#TODO: Uncomment when testing complete
+#subprocess.call(cloneAllCommand, shell=True)
 
-os.mkdir('Moss-Directory')
+#TODO: Uncomment when testing complete
+#os.chdir("../")
+#os.mkdir("Moss-Directory")
 
-#for subdir in os.walk(os.getcwd()):
-    #for i in subdir:
-        #files = os.listdir(i)
-        #print(files)
+#TODO: Uncomment when testing complete
+#os.chdir("Moss-Directory")
+#with open(rosterPath) as f:
+#    names = f.read().splitlines()
+#for name in names:
+#    os.mkdir(name)
+
+os.chdir(destinationPath + "/GitHub-Repos")
+for subdir in os.walk(os.getcwd()):
+    for i in subdir:
+        print(i)
 
 # File "/Users/CCannon/Documents/development/github-classroom-utilties/MossSetup.py", line 23, in <module>
 #    files = os.listdir(i)
