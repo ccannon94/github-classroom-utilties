0c1111816f58b3138d36e091a94667c1b11bb862
Chris Cannon <chris.c.canon@gmail.com>
2018-04-28 19:55:45 -0400

    Add inline comments to moss setup

--- MossSetup.py
+++ MossSetup.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import shutil
 
+# Validates command line arguments, outputs instructions if they are invalid
 if(len(sys.argv) == 6):
     classroomName = sys.argv[1]
     assignmentName = sys.argv[2]
@@ -15,21 +16,24 @@ else:
     print ("Invalid command line arguments, please run: python3 MossSetup.py [name of classroom][name of assignment] [path to roster] [path to CloneAssignment.py script] [path to empty directory that will hold student work]")
     sys.exit(0)
 
+# Make a directory to hold all student submissions, clone all submissions
 os.mkdir("GitHub-Repos")
 os.chdir("GitHub-Repos")
 cloneAllCommand = "python3 {} {} {} {}".format(cloneAssignmentScriptPath, classroomName, assignmentName, rosterPath)
-
 subprocess.call(cloneAllCommand, shell=True)
 
+# Make a directory to hold the MOSS formatted source files
 os.chdir(destinationPath)
 os.mkdir("Moss-Directory")
 
+# Create a directory for each student, with their GitHub username from roster
 os.chdir("Moss-Directory")
 with open(rosterPath) as f:
     names = f.read().splitlines()
 for name in names:
     os.mkdir(name)
 
+# Go through each repository, transfer every .java file to the appropriate MOS directory
 os.chdir(os.path.join(destinationPath, "GitHub-Repos"))
 for root, dir, files in os.walk(os.getcwd()):
     for file in files :
